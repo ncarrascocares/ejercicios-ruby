515c86022120e52cbff45563f4925f41d4e6b9af
Nicolas Carrasco <53540839+ncarrascocares@users.noreply.github.com>
2020-10-05 09:57:56 -0300

    Update adivina.rb
    Hi, I add an option to avoid closing the program every time the user makes a mistake

--- solutions/adivina.rb
+++ solutions/adivina.rb
@@ -11,3 +11,30 @@ end
 
 # también es posible utilizando el operador ternario
 # puts num == guess ? "Felicitaciones, ese era!" : "Lo siento, intenta nuevamente!"
+
+#generando un numero aleatorio
+#numero = rand(1000)
+#cont=1
+#solicitando numero al usuario
+#puts numero
+#puts "Ingresa un numero"
+#Opcion para limpiar los datos anteriores
+#STDOUT.flush
+#tomando el dato del usuario y pasandolo a un valor numerico
+#opcion_usuario = gets.chomp.to_i
+
+#puts opcion_usuario
+
+#if opcion_usuario == numero
+#    puts "Ganaste a la primera"
+#else
+#    puts "error...vuelve a intentarlo"
+#    while numero != opcion_usuario
+#        puts "Ingresa un numero"
+#        STDOUT.flush
+#        opcion_usuario = gets.chomp.to_i
+#        cont = cont + 1
+#    end
+    #Se envía mensaje con el contador de vueltas, se debe pasar a cadena para mostrarlo en el mensaje
+#    puts "Ganaste! lo hiciste en la vuelta numero " + cont.to_s
+#end
